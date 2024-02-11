a8ca51711b16448710e3353ad3a82324c58317a2
HuberyYang <18830279823@163.com>
2024-02-11 17:26:31 +0800

    update: 创建抽屉、列表组件

--- components/baseTable.vue
+++ components/baseTable.vue
@@ -1,9 +1,212 @@
-<script setup lang='ts'>
+<script setup>
+import { SearchOutlined } from '@ant-design/icons-vue'
 
+const {
+  totalCount,
+  currentPageNum,
+  data,
+  searchInput,
+  columns,
+  searchText,
+  searchedColumn,
+  editableData,
+  handleSearch,
+  handleReset,
+  handleDelete,
+  hanleEdit,
+  handleSave,
+  handleCancel,
+  getTagColor,
+  loading,
+} = useTable()
 </script>
 
 <template>
-  <div>index</div>
+  <!-- 表格 -->
+  <a-table
+    class="ant-table-striped"
+    bordered
+    :pagination="false"
+    :data-source="data"
+    :columns="columns"
+    :scroll="{ x: 900, y: 300 }"
+    size="middle"
+    :loading="loading"
+    :row-class-name="
+      (_record, index) => (index % 2 === 1 ? 'table-striped' : null)
+    "
+  >
+    <template #headerCell="{ column }">
+      <template v-if="column.key === 'date'">
+        <span style="color: #1890ff">日期</span>
+      </template>
+    </template>
+    <template
+      #customFilterDropdown="{
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+        column,
+      }"
+    >
+      <div style="padding: 8px">
+        <a-input
+          ref="searchInput"
+          placeholder="搜索日期"
+          :value="selectedKeys[0]"
+          style="width: 188px; margin-bottom: 8px; display: block"
+          @change="
+            (e) =>
+              setSelectedKeys(
+                e.target.value ? [e.target.value] : [],
+              )
+          "
+          @press-enter="
+            handleSearch(selectedKeys, confirm, column.dataIndex)
+          "
+        />
+        <a-button
+          type="primary"
+          size="small"
+          style="width: 90px; margin-right: 8px"
+          @click="
+            handleSearch(selectedKeys, confirm, column.dataIndex)
+          "
+        >
+          <template #icon>
+            <SearchOutlined />
+          </template>
+          搜索
+        </a-button>
+        <a-button
+          size="small"
+          style="width: 90px"
+          @click="handleReset(clearFilters, confirm)"
+        >
+          重置
+        </a-button>
+      </div>
+    </template>
+    <template #customFilterIcon="{ filtered }">
+      <SearchOutlined
+        :style="{ color: filtered ? '#108ee9' : undefined }"
+      />
+    </template>
+    <template #bodyCell="{ text, column, record }">
+      <span v-if="searchText && searchedColumn === column.dataIndex">
+        <template
+          v-for="(fragment, i) in text
+            .toString()
+            .split(
+              new RegExp(
+                `(?<=${searchText})|(?=${searchText})`,
+                'i',
+              ),
+            )"
+        >
+          <mark
+            v-if="
+              fragment.toLowerCase() === searchText.toLowerCase()
+            "
+            :key="i"
+            class="highlight"
+          >
+            {{ fragment }}
+          </mark>
+          <template v-else>{{ fragment }}</template>
+        </template>
+      </span>
+      <template
+        v-if="
+          ['weight', 'caloric', 'trainingTime'].includes(
+            column.dataIndex,
+          )
+        "
+      >
+        <div>
+          <a-input
+            v-if="editableData[record.key]"
+            v-model:value.number="
+              editableData[record.key][column.dataIndex]
+            "
+            style="margin: -5px 0"
+          />
+          <template v-else>
+            {{ text }}
+          </template>
+        </div>
+      </template>
+      <template v-else-if="column.key === 'trainingType'">
+        <span>
+          <a-tag
+            v-for="tag in record.trainingType"
+            :key="tag"
+            :color="getTagColor(tag)"
+          >
+            {{ tag }}
+          </a-tag>
+        </span>
+      </template>
+      <template v-else-if="column.key === 'operation'">
+        <div
+          :style="{
+            display: 'flex',
+            width: '190px',
+            justifyContent: 'space-around',
+          }"
+        >
+          <a-popconfirm
+            v-if="data.length"
+            title="确认删除?"
+            handle-cancel-text="取消"
+            ok-text="确认"
+            @confirm="handleDelete(record.date)"
+          >
+            <a>删除</a>
+          </a-popconfirm>
+          <div class="editable-row-operations">
+            <span v-if="editableData[record.key]">
+              <a-popconfirm
+                title="确认保存?"
+                handle-cancel-text="取消"
+                ok-text="确认"
+                @confirm="handleSave(record, record.key)"
+              >
+                <a>保存</a>
+              </a-popconfirm>
+              <a @click="handleCancel(record.key)">取消</a>
+            </span>
+            <span v-else>
+              <a @click="hanleEdit(record.key)">编辑</a>
+            </span>
+          </div>
+        </div>
+      </template>
+    </template>
+  </a-table>
+  <!-- 分页器 -->
+  <div>
+    <a-pagination
+      v-model:current="currentPageNum"
+      :total="totalCount"
+      :show-size-changer="false"
+      show-less-items
+    />
+  </div>
 </template>
 
-<style lang='scss' scoped></style>
+<style scoped>
+.highlight {
+  background-color: rgb(255, 192, 105);
+  padding: 0px;
+}
+
+.ant-table-striped :deep(.table-striped) td {
+  background-color: #fafafa;
+}
+
+.editable-row-operations a {
+  margin-right: 8px;
+}
+</style>
--- components/homePageDrawer.vue
+++ components/homePageDrawer.vue
@@ -11,12 +11,12 @@ const {
   submitLoading,
 } = useForm()
 
-const visible = ref(false)
+const isVisible = ref(false)
 </script>
 
 <template>
   <!-- button to open the drawer -->
-  <div
+  <button
     fixed
     left-0
     top="[calc(50%-5rem)]"
@@ -24,148 +24,147 @@ const visible = ref(false)
     h35
     w4
     flex="~ start content-start items-center"
-    border="1 solid #27b2f3"
     rounded-l-0
     rounded-r-5
     bg="#27b2f3"
     color-white
     shadow-blue
+    outline-none
     transition-all
     hover="w-6"
-    @click="visible = true"
+    @click="isVisible = true"
   >
     <span i-carbon-chevron-right />
-  </div>
-  <div>
-    <a-drawer
-      v-model:open="visible"
-      title="请输入内容"
-      width="720"
-      :closable="false"
-      placement="left"
-      :header-style="{ height: '40px' }"
-      :footer-style="{ textAlign: 'right', padding: '8px' }"
-      :body-style="{ padding: '5px 10px', marginBottom: '49px' }"
-    >
-      <div h-auto w-full>
-        <!-- 表单区域 -->
-        <a-form
-          ref="formRef"
-          :model="form"
-          :rules="rules"
-          layout="vertical"
-          autocomplete="off"
-          @finish="onFinish"
-          @finish-failed="onFinishFailed"
-        >
-          <a-row :gutter="16">
-            <!-- 日期选择 -->
-            <a-col :span="12">
-              <a-form-item has-feedback label="日期:" name="date">
-                <a-date-picker
-                  v-model:value="form.date"
-                  :locale="locale"
-                  style="width: 100%"
-                  placeholder="选择日期"
-                  value-format="YYYY-MM-DD"
-                  :get-popup-container="
-                    (trigger) => trigger.parentElement
-                  "
-                />
-              </a-form-item>
-            </a-col>
-            <!-- 体重 -->
-            <a-col :span="12">
-              <a-form-item
-                has-feedback
-                label="体重(kg):"
-                name="weight"
-              >
-                <a-input
-                  v-model:value.trim="form.weight"
-                  placeholder="输入体重"
-                />
-              </a-form-item>
-            </a-col>
-          </a-row>
-          <a-row :gutter="16">
-            <!-- 训练类型 -->
-            <a-col :span="12">
-              <a-form-item
-                has-feedback
-                label="训练类型:"
-                name="training"
-              >
-                <a-select
-                  v-model:value="form.training"
-                  mode="multiple"
-                  placeholder="选择训练类型"
-                  :options="trainingType"
-                />
-              </a-form-item>
-            </a-col>
-            <!-- 运动时长 -->
-            <a-col :span="12">
-              <a-form-item
-                has-feedback
-                label="运送时长(min):"
-                name="sportTime"
-              >
-                <a-input
-                  v-model:value.number.trim="form.sportTime"
-                  placeholder="输入运动时长"
-                />
-              </a-form-item>
-            </a-col>
-          </a-row>
-          <a-row :gutter="16">
-            <!-- 热量 -->
-            <a-col :span="12">
-              <a-form-item
-                has-feedback
-                label="热量(kcal)："
-                name="caloric"
-              >
-                <a-input
-                  v-model:value.number.trim="form.caloric"
-                  placeholder="输入热量"
-                />
-              </a-form-item>
-            </a-col>
-          </a-row>
-          <!-- button groups -->
-          <div
-            fixed
-            bottom-0
-            left-0
-            z-1
-            w="720px"
-            bg-white
-            b-t="1 solid #e9e9e9"
-            px-2
-            py-3
-            text-right
+    <button />
+    <div>
+      <a-drawer
+        v-model:open="isVisible"
+        title="请输入内容"
+        width="720"
+        :closable="false"
+        placement="left"
+        :header-style="{ height: '40px' }"
+        :footer-style="{ textAlign: 'right', padding: '8px' }"
+        :body-style="{ padding: '5px 10px', marginBottom: '49px' }"
+      >
+        <div h-auto w-full>
+          <!-- 表单区域 -->
+          <a-form
+            ref="formRef"
+            :model="form"
+            :rules="rules"
+            layout="vertical"
+            autocomplete="off"
+            @finish="onFinish"
+            @finish-failed="onFinishFailed"
           >
-            <a-button mr-2 @click="resetForm">
-              重置
-            </a-button>
-            <a-button type="primary">
-              index
-            </a-button>
-            <a-button
-              html-type="submit"
-              type="primary"
-              :loading="submitLoading"
+            <a-row :gutter="16">
+              <!-- 日期选择 -->
+              <a-col :span="12">
+                <a-form-item has-feedback label="日期:" name="date">
+                  <a-date-picker
+                    v-model:value="form.date"
+                    :locale="locale"
+                    style="width: 100%"
+                    placeholder="选择日期"
+                    value-format="YYYY-MM-DD"
+                    :get-popup-container="
+                      (trigger) => trigger.parentElement
+                    "
+                  />
+                </a-form-item>
+              </a-col>
+              <!-- 体重 -->
+              <a-col :span="12">
+                <a-form-item
+                  has-feedback
+                  label="体重(kg):"
+                  name="weight"
+                >
+                  <a-input
+                    v-model:value.trim="form.weight"
+                    placeholder="输入体重"
+                  />
+                </a-form-item>
+              </a-col>
+            </a-row>
+            <a-row :gutter="16">
+              <!-- 训练类型 -->
+              <a-col :span="12">
+                <a-form-item
+                  has-feedback
+                  label="训练类型:"
+                  name="training"
+                >
+                  <a-select
+                    v-model:value="form.training"
+                    mode="multiple"
+                    placeholder="选择训练类型"
+                    :options="trainingType"
+                  />
+                </a-form-item>
+              </a-col>
+              <!-- 运动时长 -->
+              <a-col :span="12">
+                <a-form-item
+                  has-feedback
+                  label="运送时长(min):"
+                  name="sportTime"
+                >
+                  <a-input
+                    v-model:value.number.trim="form.sportTime"
+                    placeholder="输入运动时长"
+                  />
+                </a-form-item>
+              </a-col>
+            </a-row>
+            <a-row :gutter="16">
+              <!-- 热量 -->
+              <a-col :span="12">
+                <a-form-item
+                  has-feedback
+                  label="热量(kcal)："
+                  name="caloric"
+                >
+                  <a-input
+                    v-model:value.number.trim="form.caloric"
+                    placeholder="输入热量"
+                  />
+                </a-form-item>
+              </a-col>
+            </a-row>
+            <!-- button groups -->
+            <div
+              fixed
+              bottom-0
+              left-0
+              z-1
+              w="720px"
+              bg-white
+              b-t="1 solid #e9e9e9"
+              px-2
+              py-3
+              text-right
             >
-              提交
-            </a-button>
+              <a-button mr-2 @click="resetForm">
+                重置
+              </a-button>
+              <a-button
+                html-type="submit"
+                type="primary"
+                ghost
+                :loading="submitLoading"
+              >
+                提交
+              </a-button>
+            </div>
+          </a-form>
+          <!-- 表格区域 -->
+          <div w-full>
+            <base-table />
           </div>
-        </a-form>
-        <!-- 表格区域 -->
-        <div w-full>
-          <!-- <Table /> -->
         </div>
-      </div>
-    </a-drawer>
-  </div>
+      </a-drawer>
+    </div>
+  </button>
 </template>
