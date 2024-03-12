4ccb63654ad5a66b54d238bf218f867ed8764317
Hub-yang <18830279823@163.com>
2024-03-12 17:19:22 +0800

    feat: 部署准备

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,7 +56,7 @@ export default defineNuxtConfig({
     head: {
       viewport: 'width=device-width,initial-scale=1',
       link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', href: '/icon.svg', sizes: 'any' },
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
