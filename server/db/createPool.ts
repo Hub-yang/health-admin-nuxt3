cb21e4b93e56b00ef9ac4d20f67c842c94f9e3af
HuberyYang <18830279823@163.com>
2024-12-06 14:05:12 +0800

    feat: update database info

--- server/db/createPool.ts
+++ server/db/createPool.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise'
 
 export const pool = mysql.createPool({
-  host: '82.157.148.19',
+  host: '101.43.145.181',
   port: 3306,
   user: 'huberyyang',
   password: '19981009ys.',
